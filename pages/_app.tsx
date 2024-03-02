9be071bc97433ff649c739aed921705ee6f9245f
Sycotic Society <thecrazydutchman2000@gmail.com>
2024-03-02 01:28:39 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ const activeChain = "cronos";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
-      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+      clientId="690261969c7023f7bfee4aae4dba4425"
       activeChain={activeChain}
     >
       <Component {...pageProps} />
